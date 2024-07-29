fb0d6a2fd3aac39f5727e24995e0a7f44656c44a
leena96 <l-maass@t-online.de>
2024-07-29 12:03:03 +0200

    # Message git push
    Veränderungen der Variablennamen:
    - frau -> female
    - isced_2 -> isced_fct
    - gkpol_kat1 -> gkpol_fct
    - p_wohnort -> p_abroad
    - wohnort_gleich -> same_lp
    - distanz_h -> distance_h
    - distanz_m -> distance_m
    - distanz_gesamt -> distance_total
    - istanz_gesamt_a -> distance_total_960
    - distanz_finish -> distance_finish
    
    Weitere Änderungen
    - Variablen bearbeitet entfernt: Bundesland (bula) und Fernbeziehung (püber 60 Minuten Fahrzeit
    
    To do's:
    Bildungskategorien möglicherweise anpassen?
    online ist bislang noch metrisch und nicht ein Faktor (Online vs. Offline 1 und 0)

--- prog/01_data_preparation.R
+++ prog/01_data_preparation.R
@@ -5,18 +5,9 @@ library(haven)
 
 # Variables --------------------------------------------------------------------
 
-#Welle 1: sd3, sd4g, sd5ezbm, sd5ezby, sd5ezbm, sd5ezby, sd7ez, sd8exbm, sd8exby, sd10, hc1pxi1,
-#          hc1pxi2, mig4, igr14
-#Welle 11 (Refreshmeint): sd5ezbm, sd5ezby, sd8e1bm, sd8e1by, sd10, mig4
-#Welle 2-11: ehc1pxg, ehc2px, ehc3px, ehc4px,igr14p1
-#Welle 3-11: ehc27pxi2,
-#Welle 1-11: job2, job7,pa2m, pa2y,pa3, sdp4, sdp5,sdp6,sdp11,hcp1i1, hcp1i2, hcp3h, hcp3m, pa9,hc13h1 inc
-
-#Mobilitätsabsichten welle 7-11: hc29, hc30, hc31
-#work-life-balance mit aufnehmwn? job23
-#Pendeln Wohnung Arbeit: Welle 1,3,5 ab 7 jährlich: job16h, job16m
+#Welle 1-11: pa2m, pa2y,pa3, hcp1i1, hcp1i2, hcp3h, hcp3m, pa9, hc13h1 inc
 #strukturelle Merkmale des Partnermarktes (Welle 1-11): sin6i3,sin6i4,sin6i5
-#Singles: Nutzung Internet/ sind 8
+
 
 # load anchor data -------------------------------------------------------------
 
@@ -32,12 +23,10 @@ df0 <-
     
     i <- str_extract(anch_file,"\\d+") %>% parse_number()
     col1 <- c("id", "wave", "cohort","migstatus",
-              "bula", "sin6i3", "sin6i4", "sin6i5", "gkpol", 
+              "bula", "sin6i3", "sin6i4", "sin6i5", "gkpol", "sdp4",
               "school", "doby_gen", "sex_gen", "age", "nkidsbio", "isced", 
-              "marstat", "relstat", "meetdur", "reldur", "homosex", "ethni", 
-              "job2", "pa3", "sdp4", "sdp6", "sdp11", "hcp1i1", "hcp1i2", "hcp3h", 
-              "hcp3m", "pa9", "inc2", "job19", "igr41p1", "sdp22", "job16h", 
-              "job16m", "hc29")
+              "marstat", "relstat", "meetdur", "reldur", "homosex",
+              "pa3", "hcp1i1", "hcp1i2", "hcp3h", "hcp3m", "inc2")
     print(anch_file)
     read_dta(unz(zipfile, filename = anch_file)) %>%
       select(any_of(col1))
@@ -47,148 +36,117 @@ df0 <-
 saveRDS(df0, file = "./data/anchor_w1-11.RDS")
 df1 <- readRDS(file = "./data/anchor_w1-11.RDS")
 
+
+
 # create Variables -------------------------------------------------------------
 
 ## Info on ego------------------------------------------------------------------
-# year
+### year of survey
 df1$year <- as.numeric(df1$wave) + 2008
 df1 %>% count(wave,year)
 
-### year of birth Geburtsjahr/Kohorte ------------------------------------------
+### year of birth and birth cohort ---------------------------------------------
 df1 %>% count(doby_gen,cohort)
 
-
-### Alter ----------------------------------------------------------------------
+### age ------------------------------------------------------------------------
 df1 %>% count(age)
 df1$age_fct <- factor(df1$age)
 
-### Geschlecht  ----------------------------------------------------------------
+### sex  -----------------------------------------------------------------------
 df1 %>% count(sex_gen)
 class(df1$sex_gen)
-df1$frau <- ifelse(df1$sex_gen == 2, 1, 0)
-df1$frau <- as.factor(df1$frau)
-
+df1$female <- ifelse(df1$sex_gen == 2, 1, 0)
+df1$female <- as.factor(df1$female)
 
 ### education: isced  ----------------------------------------------------------
 df1 %>% count(isced)
 df1$isced[df1$isced<0]<-NA
-df1$isced_2 <- case_when(df1$isced >= 0 & df1$isced <= 3 ~ "low",     
-                         df1$isced >= 4 & df1$isced <= 6 ~ "middle",
-                         df1$isced >= 7 & df1$isced <= 8 ~ "high")
-df1$isced_2 <- factor(df1$isced_2, levels = c("low","middle","high"))
+df1$isced_fct <- case_when(df1$isced >= 0 & df1$isced <= 3 ~ "low",     
+                           df1$isced >= 4 & df1$isced <= 6 ~ "middle",
+                           df1$isced >= 7 & df1$isced <= 8 ~ "high")
+df1$isced_fct <- factor(df1$isced_fct, levels = c("low","middle","high"))
 
+### migration background  ------------------------------------------------------
+df1 %>% count(migstatus)
 
-### Migration background  ------------------------------------------------------
-df1 %>% count(migstatus)# aus pairfam
-
-### Bundesland  ----------------------------------------------------------------
-table(df1$bula)
-df1$age[df1$bula<0]<-NA
-class(df1$bula)
-df1$bula <- as.factor(df1$bula)
-
-
-### Gemeindegröße (gkpol)  -----------------------------------------------------
+### municipality size: gkpol  --------------------------------------------------
+df1 %>% count(gkpol)
 df1$gkpol[df1$gkpol<0] <- NA
-df1$gkpol_kat1 <- case_when(df1$gkpol >= 1 & df1$gkpol <= 3 ~ "small",     
-                            df1$gkpol >= 4 & df1$gkpol <= 5 ~ "middle",
-                            df1$gkpol >= 6 & df1$gkpol <= 7 ~ "big")
-df1$gkpol_kat1 <- factor(df1$gkpol_kat1, levels = c("small","middle","big"))
+df1$gkpol_fct <- case_when(df1$gkpol >= 1 & df1$gkpol <= 3 ~ "small",     
+                           df1$gkpol >= 4 & df1$gkpol <= 5 ~ "middle",
+                           df1$gkpol >= 6 & df1$gkpol <= 7 ~ "big")
+df1$gkpol_fct <- factor(df1$gkpol_fct, levels = c("small","middle","big"))
 
 
 
 
+## Information on partner & relationship ---------------------------------------
 
-## Info on partner & relationship ----------------------------------------------
-
-### Lebt aktueller Partner in DE (sdp4)
-table(df1$sdp4) # 75 Partner der Befragten leben im Ausland
+### current partner lives in Germany: sdp4
+df1 %>% count(sdp4)
 df1$sdp4[df1$sdp4<0]<-NA
-df1$p_wohnort <- factor(ifelse(df1$sdp4 == 2, 1, 0))
+df1$p_abroad <- factor(ifelse(df1$sdp4 == 2, 1, 0))
 
-
-### Online/Offline Variable (based on pa3)---------------
-table(df1$pa3)
+### place of meeting online/offline: pa3 ---------------------------------------
+df1 %>% count(pa3)
 df1$pa3[df1$pa3 < 0] <- NA
+df1$online <- case_match(df1$pa3,
+                         c(7,12,13,14) ~ 1, 
+                         c(1:6,8:11) ~ 0)
+df1 %>% count(pa3,online)
 
-df1$online <- ifelse(df1$pa3==7|df1$pa3==12|df1$pa3==13|df1$pa3==14, 1, 0) %>% factor()
-df1$online2 <- case_match(df1$pa3,
-                          c(7,12,13,14) ~ 1, 
-                          c(1:6,8:11) ~ 0)
-
-# df1$offline <- factor(ifelse(df1$pa3=="1"|df1$pa3=="2"|df1$pa3=="3"|df1$pa3=="4"|df1$pa3=="5"|df1$pa3=="6"|df1$pa3=="8"|df1$pa3=="9"|df1$pa3=="10"|df1$pa3=="11", 0, df1$pa3))
-
-df1 %>% count(pa3,online,online2)
-
-### Wohnort Partner (hcp1i1)
-df1 %>% count(hcp1i1) #Kategorie 7: wohnt im selben Wohnort wie ich (n=1088)
-df1$wohnort_gleich <- case_match(df1$hcp1i1, 1 ~0,7 ~ 1)
-df1 %>% count(hcp1i1,wohnort_gleich)
+### place of residence Partner: hcp1i1
+df1 %>% count(hcp1i1) 
+df1$same_lp <- case_match(df1$hcp1i1, 1 ~ 0,7 ~ 1)
+df1 %>% count(hcp1i1,same_lp)
 
 
-### Bundesland in dem Partner wohnt (hcp1i2)
-table(df1$hcp1i2)
-df1$hcp1i2[df1$hcp1i2<0] <-NA
-df1$hcp1i2 <- as.factor(df1$hcp1i2)
+### distance to partner in hours and minutes: hcp3h & hcp3m --------------------
+df1 %>% count(hcp3h) 
 
+df1$distance_h <- df1$hcp3h #hours
+df1$distance_h[df1$distance_h < 0 ] <- NA
+df1$distance_h <- as.numeric(df1$distance_h)
+df1$distance_min <- df1$distance_h*60
 
-### Distanz zum Partner in Min und Std (hcp3h & hcp3m)
-table(df1$hcp3h) #Anmerkung: NA: -1 bis -5
+df1 %>% count(hcp3m) #minutes
+df1$distance_m <- df1$hcp3m
+df1$distance_m[df1$distance_m < 0 ] <- NA
+df1$distance_m <- as.numeric(df1$distance_m)
 
-df1$distanz_h <- df1$hcp3h
-df1$distanz_h[df1$distanz_h < 0 ] <- NA
-df1$distanz_h <- as.numeric(df1$distanz_h)
-df1$distanz_min <- df1$distanz_h*60 #Variable zum weiterarbeiten
+df1$distance_total <- df1$distance_min + df1$distance_m # merge variables
+class(df1$distance_total)
 
-table(df1$hcp3m)
-df1$distanz_m <- df1$hcp3m
-df1$distanz_m[df1$distanz_m < 0 ] <- NA
-df1$distanz_m <- as.numeric(df1$distanz_m)
-
-# Variablen zu einer zusammenfügen (Addition)
-df1$distanz_gesamt <- df1$distanz_min + df1$distanz_m
-class(df1$distanz_gesamt)
-
-df1$distanz_gesamt_a <- ifelse(df1$distanz_gesamt > 960, NA, df1$distanz_gesamt) # @Andreas: in der Arbeit habe ich die Ausreißer ausgeschlossen (orientiert habe ich mich an der längsten Strecke in Deutschland, es wurden nur Paare in DE untersucht), für Publikation wahrscheinlich etwas anders rangehen?
-table(df1$distanz_gesamt_a)
-
-
-### Fahrzeit binär unter/über 60 Minuten zum Partner (Kurz und Fernbeziehung)
-df1$distance_under60 <- ifelse(df1$distanz_gesamt <= 60, "1", "0")
-table(df1$distance_under60)
+df1$distance_total_960 <- ifelse(df1$distance_total > 960, NA, df1$distance_total)
+table(df1$distance_total_960)
 
 
 
 
 
-#################################################
+################################### new data ################################### 
 
 saveRDS(df1, file = "./data/df1.RDS")
 
-df2 <- df1 %>% select(id, wave, year, cohort, age, frau, isced, isced_2, migstatus, bula, gkpol_kat1,
-                      pa3, online,online2, 
-                      p_wohnort, wohnort_gleich, hcp1i2,
-                      distanz_gesamt, distanz_gesamt_a, distance_under60)
+df2 <- df1 %>% select(id, wave, year, cohort, age, female, isced, isced_fct, migstatus, gkpol_fct,
+                      pa3, online, p_abroad, same_lp, hcp1i2,
+                      distance_total, distance_total_960)
 saveRDS(df2, file = "./data/df2.RDS")
 
 
- 
 
-
-#ohne Welle 1
+# exclude wave 1
 df3 <- df2[df2$wave!=1,]
 saveRDS(df3, file = "./data/df3.RDS")
 
-df4 <- df3 %>% select(id, wave, year, cohort, age, frau, isced_2, migstatus, bula, gkpol_kat1,
-                      pa3, online,online2, p_wohnort, wohnort_gleich, hcp1i2,
-                      distanz_gesamt, distanz_gesamt_a, distance_under60) 
+df4 <- df3 %>% select(id, wave, year, cohort, age, female, isced_fct, migstatus, gkpol_fct,
+                      pa3, online, p_abroad, same_lp, hcp1i2,
+                      distance_total, distance_total_960) 
 saveRDS(df4, file = "./data/df4.RDS")
 
 
-#fehlende Werte raus #ohne missings bei Kennenlernort & NA
-
+# missing values removed (without missings for place of meeting & NAs)
 df_finish <- df4[!is.na(df4$online),]
-df_finish <- df_finish[!is.na(df_finish$distanz_gesamt_a),]
-df_finish <- df_finish %>% rename(distanz_finish = distanz_gesamt_a)
-
+df_finish <- df_finish[!is.na(df_finish$distance_total_960),]
+df_finish <- df_finish %>% rename(distance_finish = distance_total_960)
 saveRDS(df_finish, file = "./data/df_finish.RDS")
